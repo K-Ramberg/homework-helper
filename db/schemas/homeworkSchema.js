1f0612a79f13f6342dd943e125b40f50e700eff3
K-Ramberg <kramberg0350@gmail.com>
2018-06-04 11:47:15 -0400

    added schema for homework

---
+++ db/schemas/homeworkSchema.js
@@ -0,0 +1,17 @@
+const Schema = require('mongoose').Schema
+
+const homeworkSchema = new Schema ({
+    title: {
+        type: String,
+        required: true
+    },
+    description: String,
+    subject: String,
+    createdBy: String,
+    dueDate: {
+        type: Date,
+        default: new Date()
+    }
+})
+
+module.exports = homeworkSchema
